06294008ac93e643df0a12cab1342b63bba87ec9
LucasMede1ros <lucas.zacarias@univille.br>
2024-04-28 21:46:40 -0300

    primeiro commit

---
+++ projfso2024b/src/main/java/br/univille/projfso2024b/controller/HomeController.java
@@ -0,0 +1,20 @@
+package br.univille.projfso2024b.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@SuppressWarnings("unused")
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    @GetMapping
+    //@ResponseBody
+    public ModelAndView index(){
+        var msg = "eu nao acredito";
+        return new ModelAndView("home/index","apelido", msg);
+    }
+}
